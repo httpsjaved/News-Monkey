5d67d23e569852f1e806ed3f13e536cb3cdc2eb0
javed1225 <javed1225@hotmail.com>
2021-11-11 13:45:37 +0530

    fix permamently NavBar on the top, fix fetchMoreData page+1 in url to solve error as setPage is Asynchronous function

--- src/components/News.js
+++ src/components/News.js
@@ -65,7 +65,7 @@ const  News = (props) => {
         //setState({page:state.page + 1})
         setPage(page + 1)
 
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${this.state.page}&pageSize=${props.pageSize}`;
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json()
         setarticles(articles.concat(parsedData.articles))
@@ -80,7 +80,7 @@ const  News = (props) => {
 
         return (
             <>
-                <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+                <h1 className="text-center" style={{ margin: '35px 0px',marginTop:'90px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
                 {loading && <Spinner />}
                 <InfiniteScroll
                     dataLength={articles.length}

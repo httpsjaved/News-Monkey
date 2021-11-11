2d1c1b7764a35404e84e4dc8d08a1e970f2a36e1
javed1225 <javed1225@hotmail.com>
2021-11-11 13:28:36 +0530

    converting class base component into function base component except app.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
 
-export class NavBar extends Component {
+const NavBar = () => {
 
-    render() {
+    
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -32,7 +32,7 @@ export class NavBar extends Component {
                     </nav>
             </div>
         )
-    }
+    
 }
 
 export default NavBar
